8fdb437502ef7cfdde2e088a77d63aebd6f5e1e2
Error80 <antoninotancinco@gmail.com>
2024-11-04 14:37:56 -0500

    11/4/2024

--- Assignment3-CarServiceShop/MainWindow.xaml.cs
+++ Assignment3-CarServiceShop/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.DirectoryServices.ActiveDirectory;
+using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Assignment3_CarServiceShop
 {
@@ -31,11 +34,52 @@ namespace Assignment3_CarServiceShop
 
         public int count = 0;
 
+        /// <summary>
+        /// Method that clears the window
+        /// </summary>
+        private void ClearWindow()
+        {
+            FirstNameTextBox.Text = "";
+            LastNameTextBox.Text = "";
+            PhoneNumberTextBox.Text = "";
+            MakeComboBox.Text = "";
+            ModelTextBox.Text = "";
+            YearComboBox.Text = "";
+            ColourTextBox.Text = "";
+            EngineOilChangeCheckBox.IsChecked = false;
+            TransmissionOilChangeCheckBox.IsChecked = false;
+            AirFilterChangeCheckBox.IsChecked = false;
+            CostTextBox.Text = "";
+        }
+
+        /// <summary>
+        /// Adds to the cost depending on what check 
+        /// </summary>
+        /// <param name="service"></param>
+        /// <param name="price"></param>
+        /// <param name="tax"></param>
+        /// <returns></returns>
+        private static double AddCost(bool service, double price, double tax)
+        {
+            double finalCost = 0;
+            if (service == true) 
+            {
+                finalCost = price + (price * tax);
+
+                return finalCost;
+
+            } else
+            {
+                return 0;
+            }
+        }
+
         private ObservableCollection<Service> services = new ObservableCollection<Service>();
         public MainWindow()
         {
             InitializeComponent();
 
+            // Array of Car Makes
             string[] carMakes =
                 [
                 "Audi", 
@@ -78,66 +122,142 @@ namespace Assignment3_CarServiceShop
             CarServiceSummaryListView.ItemsSource = services;
         }
 
-        private void AddButton_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                // Determines which button got clicked
+                Button clickedButton = sender as Button;
 
-                // Get First Name from the text box
+                // Gets the name of the button
+                string buttonName = clickedButton.Name;
+
+                // Get text from the First Name Text box
                 string firstName = FirstNameTextBox.Text;
 
-                // Get Last Name from the text box
+                // Get text from the Last Name Text box
                 string lastName = LastNameTextBox.Text;
 
-                // Get Phone Number from the text box
+                // Get text from the Phone Number Text box and convert it to long
                 long phoneNumber = long.Parse(PhoneNumberTextBox.Text);
 
-                // Get Make from the dropdown menu or combo box
+                // Get chosen item from the Make dropdown menu or combo box
                 string make = MakeComboBox.Text;
 
+                // Get text from the Model Text box
                 string model = ModelTextBox.Text;
 
+                // Get text from the Colour Text box
                 string colour = ColourTextBox.Text;
 
+                // Check if the Engine Oil Change is Checked
                 bool engineOilChange = EngineOilChangeCheckBox.IsChecked == true;
 
+                // Check if the Transmission Oil Change is Checked
                 bool transmissionOilChange = TransmissionOilChangeCheckBox.IsChecked == true;
 
+                // Check if the Air Filter Change is Checked
                 bool airFilterChange = AirFilterChangeCheckBox.IsChecked == true;
 
-                double cost = 0;
-
-                if (engineOilChange == true)
-                {
-                    cost += engineOilChangePrice + (engineOilChangePrice * tax);
-                }
-
-                if (transmissionOilChange == true)
-                {
-                    cost += transmissionOilChangePrice + (transmissionOilChangePrice * tax);
-                }
-
-                if (airFilterChange == true)
-                {
-                    cost += airFilterChangePrice + (airFilterChangePrice * tax);
-                }
+                // Initialize the cost varable
+                double cost = 
+                    AddCost(engineOilChange, engineOilChangePrice, tax) +
+                    AddCost(transmissionOilChange, transmissionOilChangePrice, tax) +
+                    AddCost(airFilterChange, airFilterChangePrice, tax);
 
                 double roundedCost = Math.Round(cost, 2);
-                CostTextBox.Text = roundedCost.ToString();
+                CostTextBox.Text = "$" + roundedCost.ToString();
 
                 int year = int.Parse(YearComboBox.Text);
 
-                count++;
+                // Incrament identificationNumber by 1
                 identificationNumber++;
-                
-                Service newService = new Service(count, identificationNumber, firstName, lastName, phoneNumber, make, model, year, colour, true, true, true, roundedCost);
 
-                services.Add(newService);
-            } catch
-            {
+                if (buttonName == "AddButton")
+                {
+
+                    // Incrament count by 1
+                    count++;
+
+                    // Initalize a new Service Object
+                    Service newService = new Service(count, identificationNumber, firstName, lastName, phoneNumber, make, model, year, colour, true, true, true, roundedCost);
+
+                    // Add newService to the Serives list view
+                    services.Add(newService);
+
+                } else
+                {
+
+                    // Initalize a new Service Object
+                    Service updateService = new Service(CarServiceSummaryListView.SelectedIndex + 1, identificationNumber, firstName, lastName, phoneNumber, make, model, year, colour, true, true, true, roundedCost);
+
+
+                    // Try to replace the selected comlum in the list view
+                    try
+                    {
+                        services.Insert(CarServiceSummaryListView.SelectedIndex + 1, updateService);
+                        services.RemoveAt(CarServiceSummaryListView.SelectedIndex);
+
+                    } 
+                    
+                    // If no item is selected
+                    catch
+                    {
+                        services.RemoveAt(0);
+
+                        // Show Error Message
+                        MessageBox.Show("Please select an item from the table", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                    }
+
+                }
+
 
             }
 
+            // If Validation fails
+            catch (Exception error) 
+            {
+
+                // Show Error Message
+                MessageBox.Show(error.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void NewButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Clears all inputs
+            ClearWindow();
+        }
+
+        private void RemoveAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Emptys the services list view
+            services.Clear();
+
+            // Clears all inputs
+            ClearWindow();
+            count = 0;
+            identificationNumber = 0;
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Removes seleted item from the services list view
+                services.RemoveAt(CarServiceSummaryListView.SelectedIndex);
+                count--;
+            } catch { 
+            
+            }
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
--- Assignment3-CarServiceShop/Service.cs
+++ Assignment3-CarServiceShop/Service.cs
@@ -7,278 +7,222 @@ using System.Threading.Tasks;
 
 namespace Assignment3_CarServiceShop
 {
-    //public class Service
-    //{
-    //    private int Count 
-    //    { 
-    //        get; 
-    //        set; 
-    //    }
-    //    private int IdentificationNumber
-    //    {
-    //        get;
-    //        set;
-    //    }
-
-    //    private string FirstName
-    //    {
-    //        get;
-    //        set;
-    //    }
-
-    //    private string LastName
-    //    {
-    //        get;
-    //        set;
-    //    }
-
-    //    private int PhoneNumber
-    //    {
-    //        get;
-    //        set;
-    //    }
-    //    private string Make
-    //    {
-    //        get;
-    //        set;
-    //    }
-
-    //    private string Model
-    //    {
-    //        get;
-    //        set;
-    //    }
-
-    //    private string Colour
-    //    {
-    //        get;
-    //        set;
-    //    }
-
-    //    private int Year
-    //    { 
-    //        get;
-    //        set;
-    //    }
-
-    //    private bool EngOilChange
-    //    {
-    //        get;
-    //        set;
-    //    }
-
-    //    private bool TransOilChange
-    //    {
-    //        get;
-    //        set;
-    //    }
-
-    //    private bool AirFilterChange
-    //    {
-    //        get;
-    //        set;
-    //    }
-
-    //    private double Price
-    //    {
-    //        get;
-    //        set;
-    //    }
-
-    //    public Service(int identificationNumber, string firstName, string lastName, int phoneNumber, string make, string model, string colour, int year, bool engOilChange, bool transOilChange, bool airFilterChange, double price)
-    //    {
-    //        IdentificationNumber = identificationNumber;
-    //        FirstName = firstName;
-    //        LastName = lastName;
-    //        PhoneNumber = phoneNumber;
-    //        Make = make;
-    //        Model = model;
-    //        Colour = colour;
-    //        Year = year;
-    //        EngOilChange = engOilChange;
-    //        TransOilChange = transOilChange;
-    //        AirFilterChange = airFilterChange;
-    //        Price = price;
-    //    }
-
-    //delete below
-
-    //}
-
     public class Service
     {
+
+        // Count Property
         private int Count;
 
+        // Gets the value of the Count Property
         public int GetCount
         {
-            //
             get { return Count; }
 
-            //
+            // Checks if the value is negative
             set
             {
-                if (value <= 0)
-                    throw new ArgumentException("Count cannot be negative");
+                if (value <= -1)
+                    throw new ArgumentException("Please select an item from the table");
                 Count = value;
 
             }
         }
 
+        // Identification Property
         private int IdentificationNumber;
 
+        // Gets the value of the Identification Property
         public int GetIdentificationNumber
         {
-            //
             get { return IdentificationNumber; }
 
-            //
+            // Checks if the value is negative
             set
             {
+                IdentificationNumber = value;
                 if (value <= 0)
                     throw new ArgumentException("IdentificationNumber cannot be negative");
-                IdentificationNumber = value;
 
             }
         }
 
+        // First Name Property
         private string FirstName;
 
+
+        // Gets the value of the First Name Property
         public string GetFirstName
         {
             get { return FirstName; }
 
+            // Checks if the value is empty
             set
             {
-                if (FirstName == "")
-                    throw new ArgumentException("First Name Cannot be empty");
                 FirstName = value;
+                if (String.IsNullOrEmpty(FirstName))
+                    throw new ArgumentException("First Name Cannot be empty");
             }
         }
 
+        // Last Name Property
         private string LastName;
 
+        // Gets the value of the Last Name Property
         public string GetLastName
         {
             get { return LastName; }
 
+            // Checks if the value is empty
             set
             {
-                if (LastName == "")
-                    throw new ArgumentException("Last Name Cannot be empty");
                 LastName = value;
+                if (String.IsNullOrEmpty(LastName))
+                    throw new ArgumentException("Last Name Cannot be empty");
             }
         }
 
-        private long PhoneNumber { get; set; }
+        // Phone Number Property
+        private long PhoneNumber;
 
+        // Gets the value of the Phone Number Property
         public long GetPhoneNumber
         {
-            //
             get { return PhoneNumber; }
 
-            //
+            // Checks if the value is between 1000000000 and 9999999999
             set
             {
-                if (value <= 0)
-                    throw new ArgumentException("Phone number cannot be negative");
                 PhoneNumber = value;
+                if (value < 1000000000 || value > 9999999999)
+                    throw new ArgumentException("Phone number must be a ten digit number");
 
             }
         }
 
+        // Make Property
         private string Make;
+
+        // Gets the value of the Make Property
         public string GetMake
         {
             get { return Make; }
 
+            // Checks if the value is empty
             set
             {
-                if (Make == "")
-                    throw new ArgumentException("Make cannot be empty");
                 Make = value;
+                if (String.IsNullOrEmpty(Make))
+                    throw new ArgumentException("Make cannot be empty");
             }
         }
 
-
+        // Model Property
         private string Model;
 
+        // Gets the value of the Model Property
         public string GetModel
         {
             get { return Model; }
 
+            // Checks if the value is empty
             set
             {
-                if (Model == "")
-                    throw new ArgumentException("Model Cannot be empty");
                 Model = value;
+                if (String.IsNullOrEmpty(Model))
+                    throw new ArgumentException("Model Cannot be empty");
             }
         }
 
-
+        // Year Property
         private int Year;
 
+        // Gets the value of the Year Property
         public int GetYear
         {
-            //
             get { return Year; }
 
-            //
+            // Checks if the value is negative
             set
             {
+                Year = value;
                 if (value <= 0)
                     throw new ArgumentException("Year cannot be negative");
-                Year = value;
 
             }
         }
 
+        // Colour Property
         private string Colour;
+
+        // Gets the value of the Colour Property
         public string GetColour
         {
             get { return Colour; }
 
+            // Checks if the value is empty
             set
             {
-                if (Colour == "")
-                    throw new ArgumentException("Colour cannot be empty");
                 Colour = value;
+                if (String.IsNullOrEmpty(Colour))
+                    throw new ArgumentException("Colour cannot be empty");
             }
         }
+
+        // Engine Oil Change Property
         private bool EngOilChange
         {
             get;
             set;
         }
 
+        // Transmission Oil Property
         private bool TransOilChange
         {
             get;
             set;
         }
 
+        // Air Filter Property
         private bool AirFilterChange
         {
             get;
             set;
         }
 
+        // Price Property
         private double Price;
 
+        // Gets the value of the Price Property
         private double GetPrice
         {
-            //
             get { return Price; }
 
-            //
+            // Checks if the value is negative
             set
             {
+                Price = value;
                 if (value <= 0)
                     throw new ArgumentException("Price cannot be negative");
-                Price = value;
 
             }
         }
 
+        /// <summary>
+        /// Service Object
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="identificationNumber"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="phoneNumber"></param>
+        /// <param name="make"></param>
+        /// <param name="model"></param>
+        /// <param name="year"></param>
+        /// <param name="colour"></param>
+        /// <param name="engOilChange"></param>
+        /// <param name="transOilChange"></param>
+        /// <param name="airFilterChange"></param>
+        /// <param name="price"></param>
         public Service (
             int count,
             int identificationNumber, 
